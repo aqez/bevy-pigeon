dfdffe3c6b6a8a09f67a7ff2f9d6e3b950930bca
Tony Dwire <tony@undauntedonline.com>
2022-09-25 11:18:34 -0400

    Add feature flag for just render types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -14,10 +14,15 @@
 //! If you think other network-able types would be helpful to many users, and think it should be
 //! included here, please send a PR.
 
+#[cfg(feature = "render-types")]
 mod light;
+#[cfg(feature = "render-types")]
 mod misc;
 mod transform;
 
+#[cfg(feature = "render-types")]
 pub use light::*;
+#[cfg(feature = "render-types")]
 pub use misc::*;
+
 pub use transform::*;
